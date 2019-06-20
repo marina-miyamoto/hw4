678888872388435b169d733c34502701e887344d
Marina Miyamoto <m.miyamoto0320@gmail.com>
2019-06-21 04:27:06 +0900

    Erased Some Lists

--- Wikipedia.py
+++ Wikipedia.py
@@ -1,32 +1,25 @@
-temporary_nodes = []
-temporary_edges = []
-adjacency_list = []
 nodes = []
-with open('.gitignore/links.txt','r') as links:
-    with open('.gitignore/nicknames.txt', 'r', encoding = "utf-8_sig") as pages:
+adjacency_list = []
+
+with open('.gitignore/wiki-links.txt','r') as links:
+    with open('.gitignore/pages.txt', 'r', encoding = "utf-8_sig") as pages:
         for page in pages:
             page = page.strip().split('\t')
             nodes.append(page[0])
 
-        wiki_links = []
-        for link in links:
-            wiki_links.append(link.strip().split('\t')) 
-            print(wiki_links)
-            edge = []  
-            for i in range(len(nodes)):
-                 
-            #temporary_nodes.append(link[0])
-            #temporary_edges.append(link[1])
-                if nodes[i] == link[0]:
-                    edge.append(link[1])
-                    print(nodes[i], edge)
-                adjacency_list.append(edge)
-        #print(adjacency_list)
-            
-                
-                #for j in range(len(temporary_edges)):
-                    
-                    #print(edge)
+        tmp = []
+        
+        for line in links:
+            tmp.append(line.strip().split('\t')) 
+
+        for i in range(len(nodes)):
+            edge = []
+            for j in range(len(tmp)):
+                if nodes[i] == tmp[j][0]:
+                    edge.append(int(tmp[j][1]))
+            adjacency_list.append(edge)
+        print(adjacency_list)
+       
                    
                 
         
